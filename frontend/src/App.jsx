171c1321dc4b354fd49797dcb6cd2c77989191c5
Yash Kant Singh <yashkantsingh3@gmail.com>
2025-06-11 23:10:30 +0530

    react dom daisy UI component and react toast setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from "./pages/HomePage.jsx"
+import SignupPage from "./pages/SignupPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+import NotificationPage from "./pages/NotificationPage.jsx"
+import toast, { Toaster} from 'react-hot-toast'
 
 function App() {
   return (
-    <div className='bg-red-500 h-screen text-5xl'>
-      App
+    <div className='h-screen' data-theme="business">
+      <button onClick={() => toast.success("Hello World")}>Create a toast</button>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/signup' element={<SignupPage />}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/onboarding' element={<OnboardingPage />}></Route>
+        <Route path='/notification' element={<NotificationPage />}></Route>
+
+      </Routes>
+      <Toaster/>
     </div>
   )
 }
---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <div>
+      SignUp Page
+    </div>
+  )
+}
+
+export default SignupPage
