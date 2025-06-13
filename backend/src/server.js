948b1283a4e24f2198a6973104c76c9f8a9a95df
Yash <yashkantsingh3@gmail.com>
2025-06-13 18:41:31 +0530

    dev mode

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,12 @@ import ChatRoutes from "./routes/chat.routes.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express()
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -17,9 +20,17 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth" , AuthRoutes)
-app.use("/api/user" , UserRoutes)
-app.use("/api/chat" , ChatRoutes)
+app.use("/api/auth" , AuthRoutes);
+app.use("/api/user" , UserRoutes);
+app.use("/api/chat" , ChatRoutes);
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dict", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port - ${PORT}`);
